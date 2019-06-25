8612e5a9d46842b8c67ce69a0d035bfcf6839fab
Jiyeon Lee <32065860+jiyeoon@users.noreply.github.com>
2019-06-25 17:33:20 +0900

    Add files via upload

---
+++ front-end/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <image_picker/ImagePickerPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
